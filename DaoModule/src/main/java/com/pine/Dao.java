cfd07f290682b83d2a2055513d225a40bd5c0659
Francis Kikulwe <francissteven1@gmail.com>
2023-03-13 22:26:14 +0400

    create simple dao implementation

---
+++ DaoModule/src/main/java/com/pine/Dao.java
@@ -0,0 +1,12 @@
+package com.pine;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Dao<E> {
+    Optional<E> get(long id);
+    List<E> getAll();
+    void save(E e);
+    void update(E e, String[] params);
+    void delete(E e);
+}
---
+++ DaoModule/src/main/java/com/pine/UserDao.java
@@ -0,0 +1,41 @@
+package com.pine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class UserDao implements Dao<User> {
+    List<User> users = new ArrayList<>();
+
+    public UserDao() {
+        users.add(new User("Bella", "bella@gmail.com"));
+        users.add(new User("John", "john@alu.com"));
+    }
+
+    @Override
+    public Optional<User> get(long id) {
+        return Optional.ofNullable(users.get((int) id));
+    }
+
+    @Override
+    public List<User> getAll() {
+        return users;
+    }
+
+    @Override
+    public void save(User user) {
+        users.add(user);
+    }
+
+    @Override
+    public void update(User user, String[] params) {
+        user.setName(Objects.requireNonNull(params[0], "Name cannot be null"));
+        user.setEmail(Objects.requireNonNull(params[1], "Email cannot be null"));
+    }
+
+    @Override
+    public void delete(User user) {
+        users.remove(user);
+    }
+}
